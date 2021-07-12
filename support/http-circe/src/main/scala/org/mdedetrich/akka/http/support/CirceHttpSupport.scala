00f3dd4c5073ee820e9661ec73e8db5b3d1ed16b
Matthew de Detrich <mdedetrich@gmail.com>
2021-07-12 15:08:39 +0200

    Add back copyright info

--- support/http-circe/src/main/scala/org/mdedetrich/akka/http/support/CirceHttpSupport.scala
+++ support/http-circe/src/main/scala/org/mdedetrich/akka/http/support/CirceHttpSupport.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2015 – 2018 Paul Horn
+ * Copyright 2018 – 2021 Matthew de Detrich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.mdedetrich.akka.http.support
 
 import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
